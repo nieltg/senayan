00145d9a3043067b2b5990e437a1a809e513bf09
Daniel <nieltansah@gmail.com>
2019-06-15 22:43:20 +0700

    api-spec: entry: Add stringify()

--- packages/api-spec/src/entry.ts
+++ packages/api-spec/src/entry.ts
@@ -9,6 +9,10 @@ export namespace Entry0 {
   export const SCHEMA_URL =
     "https://nieltg.github.com/senayan/schemas/entry-0.json"
 
+  export function stringify(value: Entry0) {
+    return JSON.stringify({ ...value, $schema: SCHEMA_URL })
+  }
+
   export function verify(jsonValue: any) {
     if (jsonValue.$schema !== SCHEMA_URL) {
       return false
