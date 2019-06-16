0cab5f20eff81f6330627b1649becd6ca7289d48
Daniel <nieltansah@gmail.com>
2019-06-16 20:48:56 +0700

    devtool: SenayanMainTemplatePlugin: Adjust to Senayan infra.

--- packages/devtool/lib/SenayanMainTemplatePlugin.ts
+++ packages/devtool/lib/SenayanMainTemplatePlugin.ts
@@ -39,67 +39,60 @@ export default class SenayanMainTemplatePlugin {
         const chunkFilename = mainTemplate.outputOptions.chunkFilename
         const chunkMaps = chunk.getChunkMaps()
         return Template.asString([
-          "promises.push(Promise.resolve().then(function() {",
+          '// "1" is the signal for "already loaded"',
+          "if(!installedChunks[chunkId]) {",
           Template.indent([
-            '// "1" is the signal for "already loaded"',
-            "if(!installedChunks[chunkId]) {",
-            Template.indent([
-              "importScripts(" +
-                mainTemplate.getAssetPath(JSON.stringify(chunkFilename), {
-                  hash: `" + ${mainTemplate.renderCurrentHashCode(hash)} + "`,
-                  hashWithLength: (length: any) =>
-                    `" + ${mainTemplate.renderCurrentHashCode(
-                      hash,
-                      length
-                    )} + "`,
-                  chunk: {
-                    id: '" + chunkId + "',
-                    hash: `" + ${JSON.stringify(chunkMaps.hash)}[chunkId] + "`,
-                    hashWithLength(length: any) {
-                      const shortChunkHashMap = Object.create(null)
-                      for (const chunkId of Object.keys(chunkMaps.hash)) {
-                        if (typeof chunkMaps.hash[chunkId] === "string") {
-                          shortChunkHashMap[chunkId] = chunkMaps.hash[
+            "promises.push(self.loadAll(" +
+              mainTemplate.getAssetPath(JSON.stringify(chunkFilename), {
+                hash: `" + ${mainTemplate.renderCurrentHashCode(hash)} + "`,
+                hashWithLength: (length: any) =>
+                  `" + ${mainTemplate.renderCurrentHashCode(hash, length)} + "`,
+                chunk: {
+                  id: '" + chunkId + "',
+                  hash: `" + ${JSON.stringify(chunkMaps.hash)}[chunkId] + "`,
+                  hashWithLength(length: any) {
+                    const shortChunkHashMap = Object.create(null)
+                    for (const chunkId of Object.keys(chunkMaps.hash)) {
+                      if (typeof chunkMaps.hash[chunkId] === "string") {
+                        shortChunkHashMap[chunkId] = chunkMaps.hash[
+                          chunkId
+                        ].substr(0, length)
+                      }
+                    }
+                    return `" + ${JSON.stringify(
+                      shortChunkHashMap
+                    )}[chunkId] + "`
+                  },
+                  contentHash: {
+                    javascript: `" + ${JSON.stringify(
+                      chunkMaps.contentHash.javascript
+                    )}[chunkId] + "`
+                  },
+                  contentHashWithLength: {
+                    javascript: (length: any) => {
+                      const shortContentHashMap: { [key: string]: any } = {}
+                      const contentHash = chunkMaps.contentHash.javascript
+                      for (const chunkId of Object.keys(contentHash)) {
+                        if (typeof contentHash[chunkId] === "string") {
+                          shortContentHashMap[chunkId] = contentHash[
                             chunkId
                           ].substr(0, length)
                         }
                       }
                       return `" + ${JSON.stringify(
-                        shortChunkHashMap
+                        shortContentHashMap
                       )}[chunkId] + "`
-                    },
-                    contentHash: {
-                      javascript: `" + ${JSON.stringify(
-                        chunkMaps.contentHash.javascript
-                      )}[chunkId] + "`
-                    },
-                    contentHashWithLength: {
-                      javascript: (length: any) => {
-                        const shortContentHashMap: { [key: string]: any } = {}
-                        const contentHash = chunkMaps.contentHash.javascript
-                        for (const chunkId of Object.keys(contentHash)) {
-                          if (typeof contentHash[chunkId] === "string") {
-                            shortContentHashMap[chunkId] = contentHash[
-                              chunkId
-                            ].substr(0, length)
-                          }
-                        }
-                        return `" + ${JSON.stringify(
-                          shortContentHashMap
-                        )}[chunkId] + "`
-                      }
-                    },
-                    name: `" + (${JSON.stringify(
-                      chunkMaps.name
-                    )}[chunkId]||chunkId) + "`
+                    }
                   },
-                  contentHashType: "javascript"
-                }) +
-                ");"
-            ]),
-            "}"
+                  name: `" + (${JSON.stringify(
+                    chunkMaps.name
+                  )}[chunkId]||chunkId) + "`
+                },
+                contentHashType: "javascript"
+              }) +
+              "));"
           ]),
-          "}));"
+          "}"
         ])
       }
     )
