2b4648948a04d446690dbb7797741a6978cd1f4f
Daniel <nieltansah@gmail.com>
2019-06-17 10:57:28 +0700

    api-spec: channel: Add a sample of correct object

--- packages/api-spec/src/channel.test.ts
+++ packages/api-spec/src/channel.test.ts
@@ -5,3 +5,18 @@ import { Channel0 } from "./channel"
 test("Channel0.SCHEMA_URL is exported as string", t => {
   t.assert(typeof Channel0.SCHEMA_URL === "string")
 })
+
+test("Channel0.verify rejects object with no schema", t => {
+  t.false(Channel0.verify({}))
+})
+
+test("Channel0.verify accepts correct object", t => {
+  t.true(
+    Channel0.verify({
+      $schema: "https://nieltg.github.com/senayan/schemas/channel-0.json",
+      baseUri: "https://example.com",
+      entryConfigId: "main.entry.json",
+      mainChunkId: "main.js"
+    })
+  )
+})
--- packages/api-spec/src/channel.ts
+++ packages/api-spec/src/channel.ts
@@ -10,4 +10,8 @@ export interface Channel0 {
 export namespace Channel0 {
   export const SCHEMA_URL =
     "https://nieltg.github.com/senayan/schemas/channel-0.json"
+
+  export function verify(jsonValue: any) {
+    return Object.keys(jsonValue).length > 0
+  }
 }
