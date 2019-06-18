0da1cb0c11dd226aec686655658ebc534a57f2e1
Daniel <nieltansah@gmail.com>
2019-06-18 07:09:53 +0700

    devtool: Add ability to build

--- packages/devtool/lib/index.ts
+++ packages/devtool/lib/index.ts
@@ -1,5 +1,6 @@
-import { Compiler } from "webpack"
+import webpack, { Compiler, Configuration, Stats } from "webpack"
 
+import ExtractEntryPlugin from "./ExtractEntryPlugin"
 import FunctionModulePlugin from "./FunctionModulePlugin"
 import LoaderTargetPlugin from "./LoaderTargetPlugin"
 import SenayanTemplatePlugin from "./SenayanTemplatePlugin"
@@ -8,4 +9,22 @@ export function buildTarget(compiler: Compiler) {
   new SenayanTemplatePlugin().apply(compiler)
   new FunctionModulePlugin().apply(compiler)
   new LoaderTargetPlugin("webworker").apply(compiler)
+}
+
+export async function build(webpackConfig: Configuration) {
+  const compiler = webpack({
+    ...webpackConfig,
+    plugins: [new ExtractEntryPlugin()],
+    target: buildTarget
+  })
+
+  return await new Promise<Stats>((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(stats)
+      }
+    })
+  })
 }
